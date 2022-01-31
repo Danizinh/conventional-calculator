4ce55c2a5df3badb5b8f1bdd1564734b85205d99
Daniela <danimaltoc@gmail.com>
2022-01-31 15:59:51 -0300

    frist-commit

---
+++ calculator/script.js
@@ -0,0 +1,21 @@
+function insert(num) {
+    var numero = document.getElementById("resul").innerHTML;
+    document.getElementById("resul").innerHTML = numero + num
+}
+function clean() {
+    document.getElementById("resul").innerHTML = ""
+}
+
+function back() {
+    var resul = document.getElementById("resul").innerHTML
+    document.getElementById("resul").innerHTML = resul.substring(0, resul.length - 1)
+}
+
+function calcular() {
+    var resul = document.getElementById("resul").innerHTML
+    if (resul) {
+        document.getElementById("resul").innerHTML = eval(resul)
+    } else {
+        document.getElementById("resul").innerHTML = "..."
+    }
+}
